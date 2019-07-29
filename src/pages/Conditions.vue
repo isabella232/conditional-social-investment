ba064fc9a589b8eb305417b6d03ce56915b6754f
jakub-wojciechowski <jakub@alice.si>
2019-07-29 17:01:34 +0100

    Initial commit based on vue-material

---
+++ src/pages/Conditions.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="content">
+        <div class="md-layout">
+            <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25">
+                <stats-card data-background-color="green">
+                    <template slot="header">
+                        <md-icon>store</md-icon>
+                    </template>
+
+                    <template slot="content">
+                        <p class="category">Brexit</p>
+                        <h3 class="title">
+                            Will the Brexit happen in 2019
+                        </h3>
+
+                        <nav-tabs-table></nav-tabs-table>
+                    </template>
+
+                    <template slot="footer">
+                        <div class="stats">
+                            <md-icon>date_range</md-icon>
+                            Reported by: 0x00000
+                        </div>
+                    </template>
+                </stats-card>
+            </div>
+
+            <div
+                    class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+            >
+                <stats-card data-background-color="orange">
+                    <template slot="header">
+                        <md-icon>content_copy</md-icon>
+                    </template>
+
+                    <template slot="content">
+                        <p class="category">Used Space</p>
+                        <h3 class="title">
+                            49/50
+                            <small>GB</small>
+                        </h3>
+                    </template>
+
+                    <template slot="footer">
+                        <div class="stats">
+                            <md-icon class="text-danger">warning</md-icon>
+                            <a href="#pablo">Get More Space...</a>
+                        </div>
+                    </template>
+                </stats-card>
+            </div>
+            <div class="md-layout-item md-medium-size-50 md-xsmall-size-100 md-size-25"
+            >
+                <stats-card data-background-color="red">
+                    <template slot="header">
+                        <md-icon>info_outline</md-icon>
+                    </template>
+
+                    <template slot="content">
+                        <p class="category">Fixed Issues</p>
+                        <h3 class="title">75</h3>
+                    </template>
+
+                    <template slot="footer">
+                        <div class="stats">
+                            <md-icon>local_offer</md-icon>
+                            Tracked from Github
+                        </div>
+                    </template>
+                </stats-card>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import {
+    StatsCard,
+    NavTabsTable
+  } from "@/components";
+
+  export default {
+    components: {
+      StatsCard,
+      NavTabsTable
+    }
+  }
+</script>
