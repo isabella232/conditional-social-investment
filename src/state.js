1611b57548d185724bd5e2a27e7160922120d59f
jakub-wojciechowski <jakub@alice.si>
2019-08-01 17:37:20 +0100

    Update state

--- src/state.js
+++ src/state.js
@@ -1,61 +1,32 @@
-import Vue from 'vue';
-import Blockchain from './Blockchain';
 
-let State = {
-  debug: true,
-  // web3: {
-  //   isInjected: false,
-  //   web3Instance: null,
-  //   networkId: null,
-  //   balance: null,
-  //   error: null
-  // },
-  state: {
-    conditions: [],
-    hgContract: {},
-    projects: [
-      {
-        code: "UNEMPLOYMENT",
-        name: "Job training and acitivation program",
-        country: "Poland",
-        balance: "$5,000",
-        interests: "10%"
-      },
-      {
-        code: "CANCER",
-        name: "Breast cancer mammography analytics",
-        country: "United Kingdom",
-        balance: "$10,000",
-        interests: "3%"
-      },
-      {
-        code: "WATER",
-        name: "Building new wells in sub-saharan Africa",
-        country: "Sudan",
-        balance: "$2,500",
-        interests: "5%"
-      }
-    ],
-  },
+export const state = {
+  projects: [
+    {
+      code: "UNEMPLOYMENT",
+      name: "Job training and activation program",
+      country: "Poland",
+      balance: "$5,000",
+      interests: "10%"
+    },
+    {
+      code: "CANCER",
+      name: "Breast cancer mammography analytics",
+      country: "United Kingdom",
+      balance: "$10,000",
+      interests: "3%"
+    },
+    {
+      code: "WATER",
+      name: "Building new wells in sub-saharan Africa",
+      country: "Sudan",
+      balance: "$2,500",
+      interests: "5%"
+    }
+  ],
   projectDesc: {
-    "UNEMPLOYMENT":  "Job training and activation program",
-    "CANCER": "Breast cancer mammography analytics",
-    "WATER": "Building new wells in sub-saharan Africa"
+    "UNEMPLOYMENT":  "Job training and activation program"
   },
   balance: {
     value: 0
-  },
-  getContract() {
-    let hg = Blockchain.getHGContract();
-    Vue.set(this.state, 'hgContract', hg);
-  },
-  async getHGRegistry() {
-    let hgRegistry = await this.state.hgContract.createRegistry();
-    Vue.set(this.state, 'conditions', hgRegistry);
-  },
-  addCondition(condition) {
-    this.state.conditions.push(condition);
   }
-}
-
-export default State
+};
