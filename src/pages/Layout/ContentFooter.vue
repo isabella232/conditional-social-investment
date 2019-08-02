8a084d497dd2a84d419fbe36c2611ca4a9e0e3a8
jakub-wojciechowski <jakub@alice.si>
2019-08-02 16:05:05 +0100

    Branding

--- src/pages/Layout/ContentFooter.vue
+++ src/pages/Layout/ContentFooter.vue
@@ -4,30 +4,17 @@
       <nav>
         <ul>
           <li>
-            <a href="https://www.creative-tim.com">Creative Tim</a>
-          </li>
-          <li>
-            <a href="https://creative-tim.com/presentation">
+            <a href="https://alice.si">
               About Us
-            </a>
-          </li>
-          <li>
-            <a href="http://blog.creative-tim.com">
-              Blog
-            </a>
-          </li>
-          <li>
-            <a href="https://www.creative-tim.com/license">
-              Licenses
             </a>
           </li>
         </ul>
       </nav>
       <div class="copyright text-center">
         &copy; {{ new Date().getFullYear() }}
-        <a href="https://www.creative-tim.com/?ref=mdf-vuejs" target="_blank"
-          >Creative Tim</a
-        >, made with <i class="fa fa-heart heart"></i> for a better web
+        <a href="https://alice.si" target="_blank"
+          >Alice</a
+        >, proudly funded by the <a href="https://gnosis.io/">Gnosis</a> GECO grant
       </div>
     </div>
   </footer>
@@ -35,4 +22,8 @@
 <script>
 export default {};
 </script>
-<style></style>
+<style>
+  .footer {
+    padding: 5px;
+  }
+</style>
